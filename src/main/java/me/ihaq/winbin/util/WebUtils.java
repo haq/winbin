1088fd06c761f1ec4640eac47c4ca5e1de12b2ab
Haq <haqgamer66@gmail.com>
2018-04-30 17:59:06 -0400

    using Unirest as the HTTTP client

--- src/main/java/me/ihaq/winbin/util/WebUtils.java
+++ src/main/java/me/ihaq/winbin/util/WebUtils.java
@@ -1,14 +1,9 @@
 package me.ihaq.winbin.util;
 
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import me.ihaq.winbin.WinBin;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.Charset;
 import java.util.function.Consumer;
 
 public class WebUtils {
@@ -16,35 +11,18 @@ public class WebUtils {
     public static void makeNewPaste(Consumer<String> consumer, String contents) {
         WinBin.INSTANCE.executorService.submit(() -> {
             try {
-
-                HttpURLConnection connection = (HttpURLConnection) new URL("https://pastebin.com/api/api_post.php").openConnection();
-                connection.setRequestMethod("POST");
-                connection.setRequestProperty("User-Agent", "Mozilla/5.0");
-                connection.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
-                connection.setDoOutput(true);
-                connection.connect();
-
-                DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
-                wr.writeBytes("api_option=" + "paste" +
-                        "&api_dev_key=" + WinBin.INSTANCE.pasteBinKey +
-                        "&api_paste_private=" + 1 +
-                        "&api_paste_code=" + contents +
-                        "&api_expire_date=" + "N");
-                wr.flush();
-                wr.close();
-
-                BufferedReader r = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-
-                StringBuilder sb = new StringBuilder();
-                String line;
-                while ((line = r.readLine()) != null) {
-                    sb.append(line);
-                }
-                r.close();
-
-                consumer.accept(sb.toString());
-
-            } catch (IOException e) {
+                consumer.accept(
+                        Unirest.post("https://pastebin.com/api/api_post.php")
+                                .header("User-Agent", "Mozilla/5.0")
+                                .header("Accept-Language", "application/json")
+                                .field("api_option", "paste")
+                                .field("api_dev_key", WinBin.INSTANCE.pasteBinKey)
+                                .field("api_paste_private", 1)
+                                .field("api_paste_code", contents)
+                                .field("api_expire_date", "N")
+                                .asString().getBody()
+                );
+            } catch (UnirestException e) {
                 e.printStackTrace();
             }
 
